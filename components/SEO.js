73f3b8b739068431d28babdb76714fd7e6b858fa
Rafael Olivares <rafael.olivares@bairesdev.com>
2023-03-23 22:32:33 -0400

    add meta

--- components/SEO.js
+++ components/SEO.js
@@ -7,10 +7,6 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage, canonicalUrl 
   return (
     <Head>
       <title>{title}</title>
-      <meta
-        httpEquiv="Content-Security-Policy"
-        content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; script-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://google-analytics.com; 'unsafe-inline' 'unsafe-eval';  media-src *"
-      />
       <meta name="robots" content="follow, index" />
       <meta name="description" content={description} />
       <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />

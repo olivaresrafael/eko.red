20ae50ad76befde5b4ea5563e6effb85bfd9a72d
Rafael Olivares <rafael.olivares@bairesdev.com>
2023-04-12 11:07:44 -0400

    Eliminado articulos fake

--- pages/about.js
+++ pages/about.js
@@ -5,20 +5,19 @@ import { PageSEO } from '@/components/SEO'
 const DEFAULT_LAYOUT = 'AboutLayout'
 
 export async function getStaticProps() {
-  const Luis = await getFileBySlug('authors', ['default'])
   const Francisco = await getFileBySlug('authors', ['folivares'])
   const Rafael = await getFileBySlug('authors', ['olivaresrafael'])
   return { props: { Francisco, Rafael } }
 }
 
-export default function About({ Luis, Francisco, Rafael }) {
+export default function About({ Francisco, Rafael }) {
   return (
     <>
-      <PageSEO title={`About`} description={`About`} />
+      <PageSEO title={`Nosotros`} description={`Nosotros`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            Nosotros
           </h1>
         </div>
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">

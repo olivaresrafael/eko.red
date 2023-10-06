b443de43e0e3a0d2315df416efb3c7888e2fba59
Rafael Olivares <rafael.olivares@bairesdev.com>
2023-10-06 09:44:13 -0400

    add charts

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self' https://vitals.vercel-insights.com/v1/vitals;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vitals.vercel-insights.com/v1/vitals https://giscus.app  https://www.google-analytics.com https://www.googletagmanager.com https://google-analytics.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://fastly.jsdelivr.net/ https://vitals.vercel-insights.com/v1/vitals https://giscus.app  https://www.google-analytics.com https://www.googletagmanager.com https://google-analytics.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   child-src 'none';
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src 'self' giscus.app
 `
 
 const securityHeaders = [
@@ -29,7 +29,7 @@ const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
-    value: 'DENY',
+    value: 'SAMEORIGIN',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
   {

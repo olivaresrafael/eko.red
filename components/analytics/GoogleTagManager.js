d115a09833ac4189db8487840e796411fa5994b6
olivaresrafael <olivaresrafael@gmail.com>
2026-04-08 13:02:40 -0400

    Update GoogleTagManager.js

--- components/analytics/GoogleTagManager.js
+++ components/analytics/GoogleTagManager.js
@@ -1,8 +1,13 @@
 import Script from 'next/script'
-
 import siteMetadata from '@/data/siteMetadata'
 
 const GTMScript = () => {
+  // Extraemos el ID con un fallback por si no existe
+  const gtmId = siteMetadata?.analytics?.googleTagManagerId
+
+  // Si no hay ID, no renderizamos nada para evitar errores
+  if (!gtmId) return null
+
   return (
     <>
       <Script strategy="lazyOnload" id="gtm-script">
@@ -11,12 +16,12 @@ const GTMScript = () => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${siteMetadata.analytics.googleTagManagerId});
+          })(window,document,'script','dataLayer','${gtmId}');
         `}
       </Script>
-      <noscript id="gtm-noscript">
+      <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${siteMetadata.analytics.googleTagManagerId}`}
+          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
           height="0"
           width="0"
           style={{ display: 'none', visibility: 'hidden' }}
